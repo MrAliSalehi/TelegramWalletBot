90b7d6d632bbe4276cdfdb874f9a7905b56316e0
Qwxp <mmdjoker@yahoo.com>
2021-12-27 12:55:56 +0330

    .

--- TelegramWallet/Program.cs
+++ TelegramWallet/Program.cs
@@ -1,12 +1,10 @@
 using TelegramWallet.Classes;
-
-
- 
+Console.WriteLine("Application Running");
 var builder = WebApplication.CreateBuilder(args);
 var app =  builder.Build();
 app.UseHttpsRedirection();
 
-
+Console.WriteLine("Executing Bot");
 await new Bot().RunAsync();
 Console.WriteLine("Bot Executed");
 if (!File.Exists("Log.txt"))
