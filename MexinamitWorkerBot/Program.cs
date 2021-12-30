80f00a403a56515433cac14e11f11dac8263140d
Qwxp <mmdjoker@yahoo.com>
2021-12-30 16:28:46 +0330

    version-ing system

--- MexinamitWorkerBot/Program.cs
+++ MexinamitWorkerBot/Program.cs
@@ -1,17 +1,18 @@
 using MexinamitWorkerBot.Classes;
+using Version = MexinamitWorkerBot.Version.Version;
 
 if (!File.Exists("Log.txt"))
 {
     File.Create("Log.txt");
     Console.WriteLine("Log File Created");
 }
+var getVersion = await Version.HandelVersionAsync(new CancellationToken());
 
-Console.WriteLine("Application Running");
+Console.WriteLine($"Application Running-Vr.{getVersion}");
 var host = Host.CreateDefaultBuilder(args)
     .UseSystemd().ConfigureServices(services =>
     {
         services.AddHostedService<Bot>();
     })
     .Build();
-
 await host.RunAsync();
---
+++ MexinamitWorkerBot/Version/Version.cs
@@ -0,0 +1,25 @@
+﻿namespace MexinamitWorkerBot.Version;
+
+public static class Version
+{
+    private const double VersionNumber = 1.0;
+    private const string VersionName = "ver.doNotTouchMe";
+    public static async Task<double> HandelVersionAsync(CancellationToken ct)
+    {
+        if (!File.Exists(VersionName))
+            File.Create(VersionName);
+
+        var read = await File.ReadAllLinesAsync(VersionName, ct);
+        if (read is not { Length: > 0 }) return -1;
+        
+        var readVersion = read.ToList();
+        var canParseVersion = double.TryParse(readVersion.First(), out var version);
+        if (!canParseVersion) return -1;
+
+        if (version is not VersionNumber)
+            await File.WriteAllLinesAsync(VersionName, new List<string>() { $"{VersionNumber}" }, ct);
+        
+        return VersionNumber;
+
+    }
+}
