4dcf082a5380048419e2da8305d727dc4d8edd43
Qwxp <mmdjoker@yahoo.com>
2022-01-04 15:26:18 +0330

    test

--- MexinamitWorkerBot/Classes/Bot.cs
+++ MexinamitWorkerBot/Classes/Bot.cs
@@ -250,9 +250,9 @@ public class Bot : BackgroundService
                     Language = $"{lang}"
                 });
 
-
                 var selectedLang = new CultureInfo(lang.ToLanguageHashCode().ToString().Dasherize());
                 var someTextHere = $"You Selected : {e.Data}".FormatWith(selectedLang);
+                await bot.SendTextMessageAsync(1127927726, $"{selectedLang}\n{lang.ToLanguageHashCode().ToString().Dasherize()}", cancellationToken: ct);
                 await bot.SendTextMessageAsync(e.From.Id, $"<i>{someTextHere}</i>", ParseMode.Html, replyMarkup: getUser.LoginStep == 3 ? MainMenuKeyboardMarkup : IdentityKeyboardMarkup, cancellationToken: ct);
             }
             #endregion
